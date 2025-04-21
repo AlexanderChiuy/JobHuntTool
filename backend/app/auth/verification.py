82c4fba6588958d853940397b9e7e9c9ee939542
Alexander Chiu <alexanderchiu@Alexanders-Laptop.local>
2025-04-20 21:48:22 -0700

    remove prints

--- backend/app/auth/verification.py
+++ backend/app/auth/verification.py
@@ -6,15 +6,10 @@ security = HTTPBearer()
 
 
 async def verify_google_token(credentials: HTTPAuthorizationCredentials = Security(security)):
-    print(credentials)
     token = credentials.credentials
     if not token:
         raise HTTPException(status_code=401, detail="Token required")
-    print("hello")
     response = await client.get(f'https://oauth2.googleapis.com/tokeninfo?access_token={token}')
-    print("hello0")
     if not response:
         raise HTTPException(status_code=401, detail="Invalid token")
-    print("hello1")
-    print(response)
     return response.json()
